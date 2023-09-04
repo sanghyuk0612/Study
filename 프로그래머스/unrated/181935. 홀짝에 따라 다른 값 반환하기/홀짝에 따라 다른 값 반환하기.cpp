36b1a2ea0540113266006a4f1966d8c5b135c350
sanghyuk0612 <122794293+sanghyuk0612@users.noreply.github.com>
2023-09-04 15:13:31 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    if(n%2 == 0){
+        while(n >0){
+            answer += n*n;
+            n -=2;
+        }
+    }
+    else{
+        while(n >0){
+            answer += n;
+            n -=2;
+        }
+    }
+    return answer;
+}
