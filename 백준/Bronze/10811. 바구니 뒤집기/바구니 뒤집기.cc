862a43b72ba86d80493dbb28e9e3a041e3df4dd7
sanghyuk0612 <122794293+sanghyuk0612@users.noreply.github.com>
2024-09-02 16:25:58 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(void) {
+	int n, m;
+	cin >> n;
+	cin >> m;
+	int* basket = new int[n];
+	for (int i = 0; i < n;i++) {
+		basket[i] = i+1;
+	}
+	for (int i = 0;i < m;i++) {
+		int a1, a2;
+		cin >> a1;
+		cin >> a2;
+		a1--;
+		a2--;
+		while (a1 <= a2) {
+			int tmp;
+			tmp = basket[a1];
+			basket[a1] = basket[a2];
+			basket[a2] = tmp;
+			a1++;
+			a2--;
+		}
+	}
+	for (int i = 0; i < n;i++) {
+		cout << basket[i] << " ";
+	}
+	return 0;
+}
