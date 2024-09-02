39cd035e12ff4d8709d0674c7496a4900713ae39
sanghyuk0612 <122794293+sanghyuk0612@users.noreply.github.com>
2024-09-02 14:10:28 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    int a;
+    int b;    
+    cin >> a;
+    cin >> b;
+    cout << a+b << endl;
+    cout << a-b << endl;
+    cout << a*b << endl;
+    cout << a/b << endl;
+    cout << a%b << endl;
+}
