485170597d5e87280a37bda4ed2a65f80a3450ea
sanghyuk0612 <122794293+sanghyuk0612@users.noreply.github.com>
2025-03-05 16:57:51 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> arr(n+1);
+    arr[1]= 1;
+    arr[2]= 2;
+    for(int i=3;i<n+1;i++){
+        arr[i]=(arr[i-1]+arr[i-2])%10007;
+    }
+    cout << arr[n];
+}
