b5d6b158deef090d42f7ce15b72d7b3c417d8a74
sanghyuk0612 <122794293+sanghyuk0612@users.noreply.github.com>
2025-04-03 19:41:53 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    cout<<"  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\";
+}
