f24a3cb744dd2dd354ef89a177facd8c15c3fa35
sanghyuk0612 <122794293+sanghyuk0612@users.noreply.github.com>
2026-01-21 20:55:33 +0900

    [Silver III] Title: N과 M (7), Time: 628 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int maxDepth = 0;
+int maxN = 0;
+void back(vector<int> v, vector<int> input, int depth);
+vector<bool> selected;
+int main(void) {
+    int n1, n2;
+    cin >> n1 >> n2;
+    maxN = n1;
+    maxDepth = n2;
+    vector<int> input(n1);
+    vector<int> v(n2,0);
+    for (int i= 0 ;i <n1; i++) {
+        cin >>input[i];
+        selected.push_back(false);
+    }
+    sort(input.begin(), input.end());
+    back(v, input, 0);
+}
+
+void back(vector<int> v, vector<int> input, int depth) {
+    if (depth == maxDepth) {
+        for (int i = 0; i < v.size(); i++) {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+    }
+    else {
+        for (int i = 0; i < maxN; i++) {
+            v[depth] = input[i];
+            back(v,input, depth+1);
+        }
+    }
+}
